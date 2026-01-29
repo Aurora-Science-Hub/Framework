f25f95aaa1443fa249c82a6492b7670f50a4ee22
alexander <demosfen.spb@gmail.com>
2026-01-29 16:45:00 +0300

    +

--- tests/UnitTests/Utilities/DateTimeRangeExtensionsTests.cs
+++ tests/UnitTests/Utilities/DateTimeRangeExtensionsTests.cs
@@ -44,11 +44,11 @@ public sealed class DateTimeRangeExtensionsTests
         minutes.ShouldBeEquivalentTo(new[]
         {
             new DateTime(2021, 1, 1, 0, 0, 0),
-            new DateTime(2021, 1, 1, 1, 1, 0),
-            new DateTime(2021, 1, 1, 2, 2, 0),
-            new DateTime(2021, 1, 1, 3, 2, 0),
-            new DateTime(2021, 1, 1, 4, 2, 0),
-            new DateTime(2021, 1, 1, 5, 2, 0)
+            new DateTime(2021, 1, 1, 1, 0, 0),
+            new DateTime(2021, 1, 1, 2, 0, 0),
+            new DateTime(2021, 1, 1, 3, 0, 0),
+            new DateTime(2021, 1, 1, 4, 0, 0),
+            new DateTime(2021, 1, 1, 5, 0, 0)
         });
     }
 }
