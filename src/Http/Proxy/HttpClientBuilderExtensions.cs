52f7552e41492a4fa57e682c8502c5bac0c22ac3
Aleksei Ermilov <39774874+alex1ozr@users.noreply.github.com>
2026-02-11 23:32:58 +0300

    Implement proxy support in Http project (#18)
    * Implement proxy support in HTTP client configuration
    
    * Bump PackageBaseVersion to 10.0.3
    
    * Add IServiceCollection extension for HTTP proxy configuration
    
    * Enhance proxy configuration handling in IHttpClientBuilder extension

---
+++ src/Http/Proxy/HttpClientBuilderExtensions.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace AuroraScienceHub.Framework.Http.Proxy;
+
+/// <summary>
+/// <see cref="IHttpClientBuilder"/> extensions for configuring HTTP proxy
+/// </summary>
+public static class HttpClientBuilderExtensions
+{
+    /// <summary>
+    /// Configures the primary HTTP message handler to use a proxy if configured in <see cref="ProxyOptions"/>
+    /// </summary>
+    /// <remarks>
+    /// - Throws an <see cref="InvalidOperationException"/> if the proxy address is not configured when proxy options are provided.
+    /// - If credentials are provided, they will be used for the proxy; otherwise, default credentials will be used.
+    /// - Local addresses will bypass the proxy.
+    /// </remarks>
+    public static IHttpClientBuilder ConfigurePrimaryHttpProxyMessageHandler(this IHttpClientBuilder builder)
+    {
+        builder.ConfigurePrimaryHttpMessageHandler(serviceProvider =>
+        {
+            var proxyOptions = serviceProvider
+                .GetRequiredService<IOptions<ProxyOptions>>()
+                .Value;
+
+            if (proxyOptions.Address is null)
+            {
+                throw new InvalidOperationException(
+                    $"Proxy address is not configured for section {ProxyOptions.OptionKey}");
+            }
+
+            var credentialsProvided = !string.IsNullOrWhiteSpace(proxyOptions.UserName) &&
+                                      !string.IsNullOrWhiteSpace(proxyOptions.Password);
+
+            var handler = new HttpClientHandler
+            {
+                UseProxy = true,
+                Proxy = new WebProxy
+                {
+                    Address = proxyOptions.Address,
+                    BypassProxyOnLocal = true,
+                    UseDefaultCredentials = !credentialsProvided,
+                    Credentials = credentialsProvided
+                        ? new NetworkCredential(proxyOptions.UserName, proxyOptions.Password)
+                        : null,
+                },
+            };
+
+            return handler;
+        });
+
+        return builder;
+    }
+}
