05e5565f3b5cb6276a7f5c4abeab40d76aa0fc94
Ermilov Aleksei <alex.ozr@gmail.com>
2026-08-14 14:09:42 +0500

    Revert "Apply suggestions from code review"
    This reverts commit b0d868531381c8d92cea85f61dec4327ddeb031d.

--- tests/UnitTests/AspNetCore/Hosting/BackgroundJobBaseTests.cs
+++ tests/UnitTests/AspNetCore/Hosting/BackgroundJobBaseTests.cs
@@ -111,7 +111,7 @@ public sealed class BackgroundJobBaseTests
     private static TestJob CreateJob(JobOptions jobOptions)
         => new(
             CreateLifetime(),
-            NullLogger<BackgroundJobBase>.Instance,
+            NullLogger<TestJob>.Instance,
             Options.Create<ApplicationModuleOptionsBase>(new DefaultApplicationModuleOptions
             {
                 ScheduledJobs = [jobOptions],
